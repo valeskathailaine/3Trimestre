dd0a776644f0cbd358102cf49391eb005e46a904
valeskathailaine <127766312+valeskathailaine@users.noreply.github.com>
2023-09-13 12:27:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+document.querySelector('.tecla_pom').onclick=tocaSomPom}
+
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+document.querySelector('.tecla_clap').onclick=tocaSomClap}
+
+function tocaSomTim(){
+    document.querySelector('#som_tecla_tim').play();
+document.querySelector('.tecla_tim').onclick=tocaSomTim}
+
+            function tocaSomPuff(){
+                document.querySelector('#som_tecla_puff').play();
+                document.querySelector('.tecla_puff').onclick=tocaSomPuff}
+            
+                function tocaSomSplash(){
+                    document.querySelector('#som_tecla_splash').play();
+                    document.querySelector('.tecla_Splash').onclick=tocaSomSplash}
+            
+                    function tocaSomToim(){
+                        document.querySelector('#som_tecla_toim').play();
+                        document.querySelector('.tecla_toim').onclick=tocaSomToim}
+
+                        function tocaSomPsh(){
+                            document.querySelector('#som_tecla_psh').play();
+                            document.querySelector('.tecla_pshf').onclick=tocaSomPsh}
+                        
+                            function tocaSomTic(){
+                                document.querySelector('#som_tecla_Tic').play();
+                                document.querySelector('tecla_tic').onclick=tocaSomTic}
+                        
+                                function tocaSomTom(){
+                                    document.querySelector('#som_tecla_Tom').play();
+                                    document.querySelector('.tecla_tom').onclick=tocaSomTom}
