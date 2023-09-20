7055e7d4c10b3db2867cfc4848ebf438aa1c1744
valeskathailaine <127766312+valeskathailaine@users.noreply.github.com>
2023-09-20 12:17:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -34,4 +34,14 @@ function tocaSomTom(){
     document.querySelector('#som_tecla_tom').play();
     document.querySelector('.tecla_tom').onclick=tocaSomTom}
 
-document.querySelectorAll('.tecla'):
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+
+ listaDeTeclas[0].onclick = tocaSomPom;
+ listaDeTeclas[1].onclick = tocaSomClap;
+ listaDeTeclas[2].onclick = tocaSomTim;
+ listaDeTeclas[3].onclick = tocaSomPuff;
+ listaDeTeclas[4].onclick = tocaSomSplash;
+ listaDeTeclas[5].onclick = tocaSomToim;
+ listaDeTeclas[6].onclick = tocaSomPsh;
+ listaDeTeclas[7].onclick = tocaSomTic;
+ listaDeTeclas[8].onclick = tocaSomTom;
