d31ce3c3614ff50e77726580acaa0a2550631c7d
valeskathailaine <127766312+valeskathailaine@users.noreply.github.com>
2023-10-02 12:12:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -36,12 +36,14 @@ function tocaSomTom(){
 
     const listaDeTeclas = document.querySelectorAll('.tecla');
 
- listaDeTeclas[0].onclick = tocaSomPom;
- listaDeTeclas[1].onclick = tocaSomClap;
- listaDeTeclas[2].onclick = tocaSomTim;
- listaDeTeclas[3].onclick = tocaSomPuff;
- listaDeTeclas[4].onclick = tocaSomSplash;
- listaDeTeclas[5].onclick = tocaSomToim;
- listaDeTeclas[6].onclick = tocaSomPsh;
- listaDeTeclas[7].onclick = tocaSomTic;
- listaDeTeclas[8].onclick = tocaSomTom;
+ listaDeTeclas[contador].onclick = tocaSomPom;
+
+let contador = 0;
+while (contador <listaDeTeclas.length) {
+    listaDeTeclas[0].onclick = tocaSomPom;
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
+// Estrutura de repetição - Enquanto
