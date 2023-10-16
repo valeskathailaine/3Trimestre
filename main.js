4ce12c678b27d89e78e1feaade3ab947ced313b3
valeskathailaine <127766312+valeskathailaine@users.noreply.github.com>
2023-10-16 12:28:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,17 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
-document.querySelector('.tecla_pom').onclick=tocaSomPom}
-
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-document.querySelector('.tecla_clap').onclick=tocaSomClap}
-
-function tocaSomTim(){
-    document.querySelector('#som_tecla_tim').play();
-document.querySelector('.tecla_tim').onclick=tocaSomTim}
-
-function tocaSomPuff(){
-    document.querySelector('#som_tecla_puff').play();
-    document.querySelector('.tecla_puff').onclick=tocaSomPuff}
-            
-function tocaSomSplash(){
-    document.querySelector('#som_tecla_splash').play();
-    document.querySelector('.tecla_Splash').onclick=tocaSomSplash}
-            
-function tocaSomToim(){
-    document.querySelector('#som_tecla_toim').play();
-    document.querySelector('.tecla_toim').onclick=tocaSomToim}
-
-function tocaSomPsh(){
-    document.querySelector('#som_tecla_psh').play();
-    document.querySelector('.tecla_pshf').onclick=tocaSomPsh}
-                        
-function tocaSomTic(){
-    document.querySelector('#som_tecla_tic').play();
-    document.querySelector('.tecla_tic').onclick=tocaSomTic}
-                        
-function tocaSomTom(){
-    document.querySelector('#som_tecla_tom').play();
-    document.querySelector('.tecla_tom').onclick=tocaSomTom}
-
-    const listaDeTeclas = document.querySelectorAll('.tecla');
-
- listaDeTeclas[contador].onclick = tocaSomPom;
-
-let contador = 0;
-while (contador <listaDeTeclas.length) {
-    listaDeTeclas[0].onclick = tocaSomPom;
-    contador = contador + 1;
-
-    console.log(contador);
-
+function tocaSom(idElementoAudio) {
+    document.Selector(idElementoAudio).play();
 }
-// Estrutura de repetição - Enquanto
+const listaDeTeclas=document.querySelectorAll('.tecla');
+let contador =0;
+//Estrutura de repetição - Enquanto
+while(contador <listaDeTeclas.length){
+    const tecla=listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio'#som_$(instrumento)';
+    console.log(intrumento);
+    tecla.onclick = function(){
+        tocaSom('#som_tecla_splash');
+    };
+contador = contador + 1;
+console.log(contador);
+}
