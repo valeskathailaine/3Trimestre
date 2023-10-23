e9e6b6de5381283cfe76255c3b79e775859f99c3
valeskathailaine <127766312+valeskathailaine@users.noreply.github.com>
2023-10-23 12:26:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ function tocaSom(idElementoAudio) {
     document.Selector(idElementoAudio).play();
 }
 const listaDeTeclas=document.querySelectorAll('.tecla');
+
 let contador =0;
 //Estrutura de repetição - Enquanto
-while(contador <listaDeTeclas.length){
+for(let contador = 0;contador <listaDeTeclas;contador++) {
     const tecla=listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio'#som_$(instrumento)';
+    const idAudio= `#som_$(instrumento)`;
     console.log(intrumento);
+
     tecla.onclick = function(){
-        tocaSom('#som_tecla_splash');
+        tocaSom(idAudio);
+        
     };
-contador = contador + 1;
-console.log(contador);
 }
+tecla.classList.add('ativa');
