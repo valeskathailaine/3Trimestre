3487b88b9a48e2b2a4b926ec4303e5a1b14adb23
valeskathailaine <127766312+valeskathailaine@users.noreply.github.com>
2023-11-20 12:09:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,31 +2,28 @@
 // será o parâmetro idElementoAudio. Ou seja, o endereço da tecla será
 // procurado no html e reproduzido.
 
-function tocaSom(idElementoaudio){
-    const elemento = document.querySelector(idElementoaudio);
+function tocaSom(idElementoAudio){
+    const elemento = document.querySelector(idElementoAudio);
 // se o elemento pesquisado for igual a "inexistente", o aleta de
  // elemento não encontrado aparecerá.
-    if (elemento == null) {
-        alert('elemento não encontrado');
+    if(elemento === null){
+        alert('elemento não encontrado');}
 // se o elemnto existir, a função play daquele elemento acontecerá,ou 
 // seja, a tecla será reproduzida.
-
-    }
-
-    if (elemento != null && elemento.localName === 'áudio'){
+    if(elemento && elemento.localName === 'audio'){
     elemento.play();
     }else {
-        alert('elemento não encontrado');
+        alert('Elemento não encontrado');
     }
 }
 //querySelector encontra as informaçoes daa tecla pesquisada no documento.
-const listaDeTeclas=document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 //estrutura de repetição -enquanto
 // para que as teclas sejam repetidas de maneira infinita, criamos o
 // for, com valor  de inicio 0, e o valor e o valor final chamado de contador <
 // listaDeTeclas.length,para que a repetição seja exatamente do tamanho da nossa 
 // lista;que e de 9 teclas.
-for(let contador = 0;contador<listaDeTeclas.length;contador++) {
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     // claslist armazena todas as informações do elemento pesquisado.
     const instrumento = tecla.classList[1];
